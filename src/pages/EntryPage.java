8b97dd9dfb465a51e702355813f234aff791a6fe
Tachibana <marrybeautiful1950@gmail.com>
2024-07-29 00:03:02 +0700

    feat; entrypage

--- src/pages/EntryPage.java
+++ src/pages/EntryPage.java
@@ -1,12 +1,15 @@
 package pages;
 
-import java.awt.Color;
+
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
+// Img
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
@@ -14,25 +17,29 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import components.Entrypagepanel;
 
-import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import resource.colors.MainColor;
 import resource.environment.WindowClosingFrameEvent;
 import resource.environment.WindowEntryScreen;
+import utils.useButton;
 
 public class EntryPage extends JFrame {
     public EntryPage() {
-        JPanel epanel = new Entrypagepanel(this,"Program","page",0,0,0,0);
-        JPanel epanel2 = new Entrypagepanel(this,"Develops","member",0,0,0,0);
+        // JPanel epanel = new Entrypagepanel(this,"Program","page",0,0,0,0);
+        // JPanel epanel2 = new Entrypagepanel(this,"Develops","member",0,0,0,0);
+        JButton navToPage = new useButton().createButton("-", "Program", 0, 0, "hand", this, "page");
+        JButton navToDev = new useButton().createButton("-", "Develop", 0, 0, "hand", this, "member");
+        JButton navToExit = new useButton().createButton("-", "Exit", 0, 0, "hand", this, "exit");
         setTitle("PM 2.5 Management");
         setSize(500, 500);
         setLayout(new GridBagLayout());
 
         // Location
-        // int width = (int)(Math.ceil(gd.getDisplayMode().getWidth() / 2.5));
-        // int height = (int)(Math.ceil(gd.getDisplayMode().getHeight() / 4));
+        
         setLocation(new WindowEntryScreen().getWidthCenter(), new WindowEntryScreen().getHeightCenter());
 
         // System.out.println(width + " | " + height);
@@ -48,52 +55,51 @@ public class EntryPage extends JFrame {
             e.printStackTrace();
         }
 
-        // new useSourceImg().getSourceImage(this, getName());
 
-        // Menu Hamburger
-        // ButtonNavigate navigateToPage = new ButtonNavigate("Program", this, new
-        // Page());
-        // ButtonNavigate navigateToMember = new ButtonNavigate("Member", this, new
-        // Member());
+        JPanel panel = new JPanel(new GridLayout(3, 1));
+        panel.setBackground(MainColor.trinary());
 
-        // JButton navigateToPage = new JButton("Program");
-        // navigateToPage.addActionListener(e -> {
-        //     JFrame page = new Page();
-        //     new WindowClosingFrameEvent(this, page);
-        //     page.setVisible(true);
-        //     dispose();
-        // });
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(MainColor.primary());
 
-        // JButton navigateToMember = new JButton("Develops");
-        // navigateToMember.addActionListener(e -> {
-        //     JFrame member = new Member();
-        //     new WindowClosingFrameEvent(this, member);
-        //     member.setVisible(true);
-        //     dispose();
-        // });
-
-        // JPanel panel = new JPanel(new GridLayout(4, 2));
-        // panel.add(navigateToPage);
-        // panel.add(navigateToMember);
-        // panel.setBackground(MainColor.secondary());
-
-        // add(panel);
+        //Button manu
         GridBagConstraints gridConst = new GridBagConstraints();
         gridConst.gridx = 0;
-        gridConst.gridy = 1;
+        gridConst.gridy = 0;
         gridConst.weightx = 1.0;
-        gridConst.weighty = 0.7;
+        gridConst.weighty = 1.0;
         gridConst.fill = GridBagConstraints.BOTH;
-        gridConst.insets = new Insets(10,100,200,100);
-        add(epanel,gridConst);
+
+        panel.add(navToPage,gridConst);
+
         gridConst.gridx = 0;
         gridConst.gridy = 1;
         gridConst.weightx = 1.0;
-        gridConst.weighty = 0.7;
+        gridConst.weighty = 1.0;
         gridConst.fill = GridBagConstraints.BOTH;
-        gridConst.insets = new Insets(300,100,10,100);
-        add(epanel2,gridConst);
 
+        panel.add(navToDev,gridConst);
+        panel.add(navToExit,gridConst);
+
+        gridConst.gridx = 0;
+        gridConst.gridy = 1;
+        gridConst.weightx = 1.0;
+        gridConst.weighty = 0.5;
+        gridConst.fill = GridBagConstraints.BOTH;
+        gridConst.insets = new Insets(300,150,50,150);
+
+        add(panel,gridConst);
+        
+        //Picture
+
+        gridConst.gridx = 0;
+        gridConst.gridy = 1;
+        gridConst.weightx = 1.0;
+        gridConst.weighty = 0.5;
+        gridConst.fill = GridBagConstraints.BOTH;
+        gridConst.insets = new Insets(50,50,180,50);
+
+        add(panel2,gridConst);
 
         new WindowClosingFrameEvent(this);
         // setUndecorated(true);
--- src/utils/useButton.java
+++ src/utils/useButton.java
@@ -1,7 +1,11 @@
 package utils;
+import pages.Member;
+import pages.Page;
+import resource.environment.WindowClosingFrameEvent;
 
 // Swing
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 // AWT
 import java.awt.Color;
@@ -110,6 +114,80 @@ public class useButton {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return btn;
+    }
+    
+    public JButton createButton(String btnCase, String title, int width, int height, String cursorCase,JFrame thispage,String destpage) {
+        JButton btn = new JButton(title);
+        btn.setBorderPainted(false);
+        btn.setOpaque(true);
+        btn.setContentAreaFilled(false);
+
+        btn.setPreferredSize(new Dimension(width, height));
+
+        switch (cursorCase) {
+            case "hand":
+                btn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                break;
+
+            default:
+                btn.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+                break;
+        }
+
+        // Set Icon
+        String icon = "";
+
+        if (btnCase.equals("rain")) {
+            icon = "cloudy.png";
+
+        } else if (btnCase.equals("air")) {
+            icon = "airplane.png";
+
+        } else if (btnCase != "") {
+            icon += btnCase + ".png";
+
+        } else {
+            icon = null;
+
+        }
+
+        try (InputStream is = Footer.class.getClassLoader().getResourceAsStream("resource/images/" + icon)) {
+            if (is == null) {
+                System.out.println("Image not found");
+            } else {
+                BufferedImage iconImage = ImageIO.read(is);
+                int buttonWidth = btn.getPreferredSize().width;
+                int buttonHeight = btn.getPreferredSize().height;
+                Image resizedImage = iconImage.getScaledInstance(buttonWidth, buttonHeight, Image.SCALE_SMOOTH);
+                btn.setIcon(new ImageIcon(resizedImage));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        btn.addActionListener((e-> {
+            if (destpage.equals("page")) {
+                JFrame page = new Page();
+            new WindowClosingFrameEvent(thispage, page);
+            page.setVisible(true);
+            thispage.dispose();
+            }
+            else if (destpage.equals("member")) {
+                JFrame member = new Member();
+                new WindowClosingFrameEvent(thispage, member);
+                member.setVisible(true);
+                thispage.dispose();
+            }
+            else if (destpage.equals("exit")) {
+                new WindowClosingFrameEvent(thispage);
+                thispage.dispose();
+            }
+            else{
+                System.out.println("Page not found");
+            }
+        }));
 
         return btn;
     }
