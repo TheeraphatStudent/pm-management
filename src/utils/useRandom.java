9525122c801707930410df4757160f83515ca46b
Tachibana1950 <marrybeautiful1950@gmail.com>
2024-08-16 20:55:35 +0700

    feat: update

--- src/utils/useRandom.java
+++ src/utils/useRandom.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class useRandom {
     public int randomRange(int min, int max) {
         Random rand = new Random();
+        //แปลงค่ให้เป็นint
         int result = (int) (rand.nextInt(max - min + 1) + min);
 
         return result;
